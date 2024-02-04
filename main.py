0661e49b5da83a113a09bc21d1ecd06a3451f3fe
AbdulInvoke <abdulhafiiz@invokeisdata.com>
2024-02-04 17:05:19 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import chromedriver_binary
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
